e0fd50cd9b62a2070ee74f930bb06b1abeabf29f
Nathaniel Bloomfield <nathaniel.bloomfield@unimelb.edu.au>
2022-11-03 18:43:15 +1100

    fixed bug

--- copybackresults.bash
+++ copybackresults.bash
@@ -1,1 +1,2 @@
- rsync -avzh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/FixMatch-pytorch2/results .
+rsync -avzh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/FixMatch-pytorch2/results .
+rsync -avzh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/FixMatch-pytorch2/*.out .
